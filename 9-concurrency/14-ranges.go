d45c8fd020739083ac713d006f9956826305aa35
Diwakar Singh <diwakarsingh052@gmail.com>
2025-09-03 17:28:27 +0530

    .

---
+++ 9-concurrency/14-ranges.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// copy the whole program,
+//but instead of having one goroutine to receive, create three goroutines to receive using ranges
+
+func main() {
+	wg := new(sync.WaitGroup)
+
+	ch := make(chan int, 5)
+	wg.Go(func() {
+		for i := 1; i <= 5; i++ {
+			ch <- i
+		}
+		fmt.Println("all values sent")
+		// sends a signal to stop the range
+		close(ch)
+		// close signal range that no more values be sent
+		//and it can stop after receiving remaining values
+		//ch <- 10 // once the channel is closed, we can't send more values to it
+
+	})
+
+	wg.Go(func() {
+
+		// it would run infinitely, channel needs to be closed to stop this range
+		// if channel is closed range will receive remaining values and stop
+		for i := range ch { // range over channel is a receive operation
+			fmt.Println(i)
+		}
+	})
+
+	wg.Wait()
+}
