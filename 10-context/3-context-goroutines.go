583f8f9b75f6da4cf2c2b4fdeaf63dfcfbfc32c6
Diwakar Singh <diwakarsingh052@gmail.com>
2025-09-08 17:59:04 +0530

    .

--- 10-context/3-context-goroutines.go
+++ 10-context/3-context-goroutines.go
@@ -14,8 +14,15 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
+	// sender goroutine
 	wg.Go(func() {
-		x := slowFunc(ctx)
+		x := slowFuncWithoutSelect(ctx)
+
+		// unbuffered channel channel has one condition which is receiver is must
+		// so if main goroutine moved on then there would be no receiver for the value
+		// so using select we will only send the value if the receiver is ready
+		// if ctx.Done happens then it means the receiver is not present anymore
+		// and we should not send the value
 		select {
 		case <-ctx.Done():
 			fmt.Println("sender: context is done, time up, and moving on")
@@ -26,6 +33,7 @@ func main() {
 
 	})
 
+	//receiver goroutine (main)
 	select {
 	// if this case is true then it means the goroutine slowFunc() is not completed
 	case <-ctx.Done():
@@ -45,10 +53,20 @@ func slowFunc(ctx context.Context) int {
 	time.Sleep(2 * time.Second)
 	fmt.Println("slowFunc() ran and changed 10 files")
 	select {
+	// if ctx.Done happens it means the receiver is not present anymore
+	// so we are rolling back the changes to avoid an inconsistent state
 	case <-ctx.Done():
 		fmt.Println("rolling back all the changes as the context is done")
 		return 0
 	default:
 	}
+
+	// if ctx have time left then we are returning the value
+	return 10
+}
+
+func slowFuncWithoutSelect(ctx context.Context) int {
+	time.Sleep(2 * time.Second)
+	fmt.Println("slowFunc() ran and changed 10 files")
 	return 10
 }
