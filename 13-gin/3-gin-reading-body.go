85c962c12ad7ce74e91cd15bd29d6dfc9a794f39
Diwakar Singh <diwakarsingh052@gmail.com>
2025-09-15 16:43:43 +0530

    .

--- 13-gin/3-gin-reading-body.go
+++ 13-gin/3-gin-reading-body.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
 /*
 	{
 	  "id": 1,
@@ -9,6 +17,14 @@ package main
 	  "created_at": "2025-09-13T10:30:00Z"  // time.Time
 	}
 */
+
+type user struct {
+	UserId    int       `json:"id" binding:"required"`
+	Name      string    `json:"name"  binding:"required,min=3,max=50"`
+	Email     string    `json:"email" binding:"required,email"`
+	Age       int       `json:"age" binding:"required,gte=18,lte=120"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 /*
 curl -X POST "http://localhost:8080/users" \
@@ -29,19 +45,25 @@ curl -X POST "http://localhost:8080/users" \
 
 // print the user locally
 func main() {
-
+	r := gin.New()
+	r.POST("/users", createUser)
 	// read the body
 	//parse the body and store json it in a struct
 	// c.ShouldBindJSON(&struct)
+	r.Run(":8080")
 }
 
 func createUser(c *gin.Context) {
 	var user user
+	//ShouldBindJSON(&user)
+	// Reading the body
+	// Convert json to type
+	// validation of the data
 	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	fmt.Printf("Received user: %+v\n", user)
-	c.JSON(200, user)
+	c.JSON(http.StatusOK, user)
 }
---
+++ small-app/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+// Goal: User would send a userId and we would look up the user and return the user's details'
+
+/*
+Graceful Shutdown:
+
+	Once requested, no new requests will be accepted.
+	Existing requests will be completed, but a certain amount of time will be allowed for them to complete.
+*/
+func main() {
+
+	// overriding the default http server, with timeout values and other configurations
+	api := http.Server{
+		Addr:         ":" + "3000",
+		ReadTimeout:  8000 * time.Second,
+		WriteTimeout: 800 * time.Second,
+		IdleTimeout:  800 * time.Second,
+	}
+
+	serverError := make(chan error)
+	// starting the server in a goroutine, so it doesn't block the main'
+	go func() {
+		err := api.ListenAndServe()
+		serverError <- err
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+
+	//singal.Notify registers the given channel to receive notifications of the specified signals.
+	signal.Notify(shutdown, os.Interrupt, os.Kill)
+
+	// using select to wait for either a server error or a shutdown signal
+	// if a shutdown signal is received, we will gracefully shutdown the server
+	// select would block until either a server error or a shutdown signal is received
+	select {
+	case err := <-serverError:
+		log.Println(err)
+		return
+	case <-shutdown:
+		log.Println("Gracefully Shutting down server...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		//Shutdown gracefully shuts down the server without interrupting any active connections.
+		//Shutdown works by first closing all open listeners, then closing all idle connections,
+		err := api.Shutdown(ctx)
+		if err != nil {
+
+			// forceful shutdown
+			err := api.Close()
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
+		}
+		return
+	}
+
+}
